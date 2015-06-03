389eda9682c7aa13ee2513069a5db877fb8f9eec
David Parry <david.parry@suranyami.com>
2015-06-03 17:32:36 +1000

    use postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141009231918) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "things", force: true do |t|
     t.string   "name"
     t.integer  "age"
